a021523ef8c45651e6befd7b19201ef005e342d6
vicfreitax <victoriafreitax@gmail.com>
2024-04-13 18:37:23 -0300

    Primeira Commit

---
+++ src/main/java/com/generation/lojadegames/model/Produtos.java
@@ -0,0 +1,65 @@
+package com.generation.lojadegames.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produtos {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é Obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo deve conter entre 5 e 100 caracteres." )
+	private String titulo;
+	
+	@NotBlank(message = "O atributo descrição é obrigatório!")
+	@Size(min = 10, max = 1000, message="A descrição deve conter entre 10 e 1000 caracteres")
+	private String descricao;
+	
+	@NotBlank(message = "O atributo ano é obrigatório!")
+	private String ano;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+
+	
+	
+}
